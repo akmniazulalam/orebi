308b79ae7ab8b48507c6975238704d65b0ba5d16
niazulalam <niazulalam097@gmail.com>
2025-07-01 13:36:55 +0600

    Bottom Header Added

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -2,9 +2,17 @@ import Container from "../Container";
 import Image from "./../Image";
 import Logo from "../../assets/logo.png";
 import Flex from "../Flex";
+import MenuIcon from "../../assets/icons/MenuIcon";
+import Heading from "../Heading";
+import { FaSearch } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { IoMdArrowDropdown } from "react-icons/io";
+
 
 const Header = () => {
   return (
+    <>
     <section className="py-7">
       <Container>
         <Flex className={"justify-center relative"}>
@@ -21,6 +29,26 @@ const Header = () => {
         </Flex>
       </Container>
     </section>
+    <section className="py-6 bg-bHeaderBg">
+      <Container>
+        <Flex className={"justify-between"}>
+          <Flex>
+            <MenuIcon/>
+            <Heading className={"font-dmSans text-[14px] text-[#262626] pl-3"} text={"Shop by Category"} as={"h4"}/>
+          </Flex>
+          <div className="relative">
+            <input type="search" placeholder="Search Products" className="placeholder:text-[#C4C4C4] placeholder:font-dmSans placeholder:text-[14px] p-5 bg-white w-[600px] focus:outline-0"/>
+            <span className="absolute top-1/2 right-4 -translate-y-1/2"><FaSearch/></span>
+          </div>
+          <Flex>
+            <FaUser className="text-[#262626]"/>
+            <IoMdArrowDropdown className="text-[#262626]"/>
+            <FaShoppingCart className="text-[#262626] ml-6"/>
+          </Flex>
+        </Flex>
+      </Container>
+    </section>
+    </>
   );
 };
 
