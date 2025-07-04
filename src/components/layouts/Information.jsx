0bd3b952ce40212b84fabaa6387fa57d593475f7
niazulalam <niazulalam097@gmail.com>
2025-07-04 23:13:01 +0600

    Right formatting

--- src/components/layouts/Information.jsx
+++ src/components/layouts/Information.jsx
@@ -1,31 +1,43 @@
-import React from 'react'
-import Container from '../Container'
+import React from "react";
+import Container from "../Container";
 import { PiNumberTwoBold } from "react-icons/pi";
 import { FaTruck, FaUndoAlt } from "react-icons/fa";
-import Flex from './../Flex';
-import Heading from '../Heading';
+import Flex from "./../Flex";
+import Heading from "../Heading";
 
 const Information = () => {
   return (
-    <div className='border-b-2 border-infoBg pt-5 pb-8'>
-        <Container>
-           <Flex className={"justify-between"}>
-            <Flex className={"gap-x-2"}>
-              <PiNumberTwoBold className='text-[22px] text-menuHeading'/>
-              <Heading text={"Two years warranty"} as={"h3"} className={"font-dmSans text-base text-footerTexts"}/>
-            </Flex>
-            <Flex className={"gap-x-2"}>
-              <FaTruck className='text-[22px] text-menuHeading'/>
-              <Heading text={"Free shipping"} as={"h3"} className={"font-dmSans text-base text-footerTexts"}/>
-            </Flex>
-            <Flex className={"gap-x-3"}>
-              <FaUndoAlt className='text-[22px] text-menuHeading'/>
-              <Heading text={"Return policy in 30 days"} as={"h3"} className={"font-dmSans text-base text-footerTexts"}/>
-            </Flex>
-           </Flex>
-        </Container>
+    <div className="border-b-2 border-infoBg pt-5 pb-8">
+      <Container>
+        <Flex className={"justify-between"}>
+          <Flex className={"gap-x-2"}>
+            <PiNumberTwoBold className="text-[22px] text-menuHeading" />
+            <Heading
+              text={"Two years warranty"}
+              as={"h3"}
+              className={"font-dmSans text-base text-footerTexts"}
+            />
+          </Flex>
+          <Flex className={"gap-x-2"}>
+            <FaTruck className="text-[22px] text-menuHeading" />
+            <Heading
+              text={"Free shipping"}
+              as={"h3"}
+              className={"font-dmSans text-base text-footerTexts"}
+            />
+          </Flex>
+          <Flex className={"gap-x-3"}>
+            <FaUndoAlt className="text-[22px] text-menuHeading" />
+            <Heading
+              text={"Return policy in 30 days"}
+              as={"h3"}
+              className={"font-dmSans text-base text-footerTexts"}
+            />
+          </Flex>
+        </Flex>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Information
+export default Information;
