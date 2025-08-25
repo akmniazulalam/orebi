7ab473ac273ecae60678576d906744134893e52d
niazulalam <niazulalam097@gmail.com>
2025-08-25 23:42:53 +0600

    Code short by using map

--- src/components/CategoriesMenu.jsx
+++ src/components/CategoriesMenu.jsx
@@ -2,26 +2,22 @@ import React from 'react'
 
 const CategoriesMenu = ({isOpen}) => {
   return (
-    <div className={`w-[263px] z-10 absolute left-0 transition-all duration-300 bg-menuHeading ${isOpen ? "top-[40px] opacity-100" : "top-[20px] opacity-0"}`}>
-        <div className="py-[19px] px-[21px] border-b-2 border-[#2D2D2D] flex justify-between items-center font-dmSans text-sm text-white opacity-70 hover:opacity-100 hover:font-bold hover:pl-9 transition-all duration-300">
-            Accesories <span>&gt;</span>
-        </div>
-        <div className="py-[19px] px-[21px] border-b-2 border-[#2D2D2D] flex justify-between items-center font-dmSans text-sm text-white opacity-70 hover:opacity-100 hover:font-bold hover:pl-9 transition-all duration-300">
-            Furniture <span>&gt;</span>
-        </div>
-        <div className="py-[19px] px-[21px] border-b-2 border-[#2D2D2D] flex justify-between items-center font-dmSans text-sm text-white opacity-70 hover:opacity-100 hover:font-bold hover:pl-9 transition-all duration-300">
-            Electronics <span>&gt;</span>
-        </div>
-        <div className="py-[19px] px-[21px] border-b-2 border-[#2D2D2D] flex justify-between items-center font-dmSans text-sm text-white opacity-70 hover:opacity-100 hover:font-bold hover:pl-9 transition-all duration-300">
-            Clothes <span>&gt;</span>
-        </div>
-        <div className="py-[19px] px-[21px] border-b-2 border-[#2D2D2D] flex justify-between items-center font-dmSans text-sm text-white opacity-70 hover:opacity-100 hover:font-bold hover:pl-9 transition-all duration-300">
-            Bags <span>&gt;</span>
-        </div>
-        <div className="py-[19px] px-[21px] border-b-2 border-[#2D2D2D] flex justify-between items-center font-dmSans text-sm text-white opacity-70 hover:opacity-100 hover:font-bold hover:pl-9 transition-all duration-300">
-            Home appliances <span>&gt;</span>
-        </div>
+    <>
+    {isOpen && (
+        <div className={`w-[263px] z-10 absolute left-0 transition-all duration-300 bg-menuHeading animate-slide`}>
+        {["Accesories", "Furniture", "Electronics", "Clothes", "Bags", "Home appliances"].map(
+            (item) => (
+              <div
+                key={item}
+                className="py-[19px] px-[21px] border-b-2 border-[#2D2D2D] flex justify-between items-center font-dmSans text-sm text-white opacity-70 hover:opacity-100 hover:font-bold hover:pl-9 transition-all duration-300"
+              >
+                {item} <span>&gt;</span>
+              </div>
+            )
+          )}
     </div>
+    )}
+    </>
   )
 }
 
