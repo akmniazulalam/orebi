a1b7f8c3ed89d9222db64acca7c0a1c6aa64be8a
akmniazulalam <niazulalam097@gmail.com>
2026-04-26 22:31:34 +0600

    padding fix

--- src/components/CategoriesMenu.jsx
+++ src/components/CategoriesMenu.jsx
@@ -4,12 +4,12 @@ const CategoriesMenu = ({isOpen}) => {
   return (
     <>
     {isOpen && (
-        <div className={`w-[263px] z-10 absolute left-0 transition-all duration-300 bg-menuHeading animate-slide`}>
+        <div className={`w-65.75 z-10 absolute left-0 transition-all duration-300 bg-menuHeading animate-slide`}>
         {["Accesories", "Furniture", "Electronics", "Clothes", "Bags", "Home appliances"].map(
             (item) => (
               <div
                 key={item}
-                className="py-[19px] px-[21px] border-b-2 border-[#2D2D2D] flex justify-between items-center font-dmSans text-sm text-white opacity-70 hover:opacity-100 hover:font-bold hover:pl-9 transition-all duration-300"
+                className="py-4.75 px-5.25 border-b-2 border-[#2D2D2D] flex justify-between items-center font-dmSans text-sm text-white opacity-70 hover:opacity-100 hover:font-bold hover:pl-9 transition-all duration-300"
               >
                 {item} <span>&gt;</span>
               </div>
