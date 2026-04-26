a07c14753ba47d719c5472d4d2302f13f0a874a7
akmniazulalam <niazulalam097@gmail.com>
2026-04-27 02:35:02 +0600

    select options use from shadcn

--- src/components/pages/ProductDetails.jsx
+++ src/components/pages/ProductDetails.jsx
@@ -7,6 +7,15 @@ import Intro from "../Intro";
 import Heading from "../Heading";
 import { IoMdStar } from "react-icons/io";
 import { FaCircle } from "react-icons/fa";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const ProductDetails = () => {
   return (
@@ -43,32 +52,96 @@ const ProductDetails = () => {
           </Flex>
           <hr className="text-[#d8d8d8]" />
           <Flex className={"my-8 items-center gap-x-11"}>
-            <Heading as={"h4"} className={"uppercase font-dmSans font-bold text-sm"} text={"color:"} />
+            <Heading
+              as={"h4"}
+              className={"uppercase font-dmSans font-bold text-sm"}
+              text={"color:"}
+            />
             <Flex className={"gap-x-3 items-center"}>
-                <FaCircle className="text-[#979797] w-5 h-5"/>
-                <FaCircle className="text-[#FF8686] w-5 h-5"/>
-                <FaCircle className="text-[#7ED321] w-5 h-5"/>
-                <FaCircle className="text-[#B6B6B6] w-5 h-5"/>
-                <FaCircle className="text-[#15CBA5] w-5 h-5"/>
+              <FaCircle className="text-[#979797] w-5 h-5 cursor-pointer hover:scale-135 transition-all duration-300 object-cover" />
+              <FaCircle className="text-[#FF8686] w-5 h-5 cursor-pointer hover:scale-135 transition-all duration-300 object-cover" />
+              <FaCircle className="text-[#7ED321] w-5 h-5 cursor-pointer hover:scale-135 transition-all duration-300 object-cover" />
+              <FaCircle className="text-[#B6B6B6] w-5 h-5 cursor-pointer hover:scale-135 transition-all duration-300 object-cover" />
+              <FaCircle className="text-[#15CBA5] w-5 h-5 cursor-pointer hover:scale-135 transition-all duration-300 object-cover" />
             </Flex>
           </Flex>
           <Flex className={"my-8 items-center gap-x-16"}>
-            <Heading as={"h4"} className={"uppercase font-dmSans font-bold text-sm"} text={"size:"} />
-            <select>
-                <option>Select Color</option>
-                <option>XL</option>
-            </select>
+            <Heading
+              as={"h4"}
+              className={"uppercase font-dmSans font-bold text-sm"}
+              text={"size:"}
+            />
+            <Select className={"rounded-none!"}>
+              <SelectTrigger className="w-36 h-10 rounded-none">
+                <SelectValue placeholder="Select size" />
+              </SelectTrigger>
+
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Sizes</SelectLabel>
+
+                  <SelectItem value="xs">Extra Small</SelectItem>
+                  <SelectItem value="sm">Small</SelectItem>
+                  <SelectItem value="md">Medium</SelectItem>
+                  <SelectItem value="lg">Large</SelectItem>
+                  <SelectItem value="xl">Extra Large</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
           </Flex>
           <Flex className={"my-8 items-center gap-x-6"}>
-            <Heading as={"h4"} className={"uppercase font-dmSans font-bold text-sm"} text={"quantity:"} />
-            <Flex className={"gap-x-3 items-center"}>
-                <FaCircle className="text-[#979797] w-5 h-5"/>
-                <FaCircle className="text-[#FF8686] w-5 h-5"/>
-                <FaCircle className="text-[#7ED321] w-5 h-5"/>
-                <FaCircle className="text-[#B6B6B6] w-5 h-5"/>
-                <FaCircle className="text-[#15CBA5] w-5 h-5"/>
-            </Flex>
+            <Heading
+              as={"h4"}
+              className={"uppercase font-dmSans font-bold text-sm"}
+              text={"quantity:"}
+            />
+            <div className="border py-1.5 px-5 w-36 flex gap-x-10 font-dmSans text-header">
+              <button>-</button>
+              <h6>1</h6>
+              <button>+</button>
+            </div>
           </Flex>
+          <hr className="text-[#d8d8d8]" />
+          <Flex className={"my-6 items-center gap-x-6"}>
+            <Heading
+              as={"h4"}
+              className={"uppercase font-dmSans font-bold text-sm"}
+              text={"status:"}
+            />
+            <p className="text-header font-dmSans">In stock</p>
+          </Flex>
+          <hr className="text-[#d8d8d8]" />
+          <Flex className={"my-8 items-center gap-x-4"}>
+            <button className="py-3 px-10 text-menuHeading text-base font-bold font-dmSans hover:bg-menuHeading hover:text-white transition-all duration-300 cursor-pointer border-2 border-menuHeading">
+              Add to Wish List
+            </button>
+            <button className="py-3 px-10 text-menuHeading text-base font-bold font-dmSans hover:bg-menuHeading hover:text-white transition-all duration-300 cursor-pointer border-2 border-menuHeading">
+              Add to Cart
+            </button>
+          </Flex>
+          <hr className="text-[#d8d8d8]" />
+          <Flex className={"my-6 items-center justify-between gap-x-6"}>
+            <Heading
+              as={"h4"}
+              className={"uppercase font-dmSans font-bold text-sm"}
+              text={"features & details"}
+            />
+            <span className="text-black font-dmSans">+</span>
+          </Flex>
+          <hr className="text-[#d8d8d8]" />
+          <Flex className={"my-6 items-center justify-between gap-x-6"}>
+            <Heading
+              as={"h4"}
+              className={"uppercase font-dmSans font-bold text-sm"}
+              text={"shipping & returns"}
+            />
+            <span className="text-black font-dmSans">+</span>
+          </Flex>
+          <hr className="text-[#d8d8d8]" />
+          <p className="font-dmSans text-base text-header leading-7 mt-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
         </div>
       </Container>
     </>
