ca73c43d3deafc33dd6ea2f7db7fc723f594d840
akmniazulalam <niazulalam097@gmail.com>
2026-04-27 00:59:49 +0600

    single product page add

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,14 +5,14 @@ import Container from "./Container";
 
 const Intro = (props) => {
   return (
-    <section className="py-[125px]">
+    <section className="py-30">
       <Container>
         <Heading
           text={props.text}
           className={"font-dmSans font-bold text-5xl text-menuHeading"}
           as={"h1"}
         />
-        <p className="font-dmSans text-[12px] text-header pt-[22px]">Home  &gt;  {props.pText}</p>
+        <p className="font-dmSans text-[12px] text-header pt-5.5">Home  &gt;  {props.pText}</p>
       </Container>
     </section>
   );
--- src/components/pages/ProductDetails.jsx
+++ src/components/pages/ProductDetails.jsx
@@ -1,17 +1,50 @@
-import React from 'react'
-import Container from '../Container'
-import Image from '../Image'
-import cup from '/src/assets/cup.png'
-import Flex from '../Flex'
+import React from "react";
+import Container from "../Container";
+import Image from "../Image";
+import cup from "/src/assets/cup.png";
+import Flex from "../Flex";
+import Intro from "../Intro";
+import Heading from "../Heading";
+import { IoMdStar } from "react-icons/io";
 
 const ProductDetails = () => {
   return (
-    <Container>
-        <Flex className={"flex-wrap gap-5"}>
-            <Image src={cup}/>
-        </Flex>
-    </Container>
-  )
-}
+    <>
+      <Intro text={"Single Product"} pText={"Single Product"} />
+      <Container>
+        <div className={"grid grid-cols-2 grid-rows-2 gap-9 mb-10"}>
+          <Image src={cup} className={"w-full"} />
+          <Image src={cup} className={"w-full"} />
+          <Image src={cup} className={"w-full"} />
+          <Image src={cup} className={"w-full"} />
+        </div>
+        <div className="w-1/2">
+          <Heading
+            as={"h3"}
+            className={"text-3xl font-bold font-dmSans"}
+            text={"Product"}
+          />
+          <Flex className={"gap-x-5 items-center my-3"}>
+            <Flex>
+              <IoMdStar className="text-[#FFD881]" />
+              <IoMdStar className="text-[#FFD881]" />
+              <IoMdStar className="text-[#FFD881]" />
+              <IoMdStar className="text-[#FFD881]" />
+              <IoMdStar className="text-[#FFD881]" />
+            </Flex>
+            <span className="text-sm font-dmSans text-header">1 Review</span>
+          </Flex>
+          <Flex className={"gap-x-5 items-center mb-3"}>
+            <span className="text-header text-base font-dmSans line-through">
+              $88.00
+            </span>
+            <span className="text-[20px] font-dmSans font-bold">$44.00</span>
+          </Flex>
+          <hr className="text-[#d8d8d8]" />
+        </div>
+      </Container>
+    </>
+  );
+};
 
-export default ProductDetails
+export default ProductDetails;
