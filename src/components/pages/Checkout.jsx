05d951aa3b3424b4e142d2fdb8078ae1aa29ce5c
akmniazulalam <niazulalam097@gmail.com>
2026-04-29 23:50:19 +0600

    checkout page intro add

--- src/components/pages/Checkout.jsx
+++ src/components/pages/Checkout.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Intro from '../Intro'
+import Container from '../Container'
 
 const Checkout = () => {
   return (
     <>
     <Intro pText={"Checkout"} text={"Checkout"} />
+    <Container>
+      <p className='text-header text-base font-dmSans leading-7.5'>Have a coupon? <span className='text-menuHeading'>Click here to enter your code</span></p>
+    </Container>
     </>
   )
 }
