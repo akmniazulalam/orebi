afa8970cfe5285b2784e181a7d969d7bc096fb94
akmniazulalam <niazulalam097@gmail.com>
2026-04-29 23:59:28 +0600

    input field add in billing details

--- src/components/pages/Checkout.jsx
+++ src/components/pages/Checkout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Intro from '../Intro'
 import Container from '../Container'
 import Heading from '../Heading'
+import Flex from '../Flex'
 
 const Checkout = () => {
   return (
@@ -9,7 +10,12 @@ const Checkout = () => {
     <Intro pText={"Checkout"} text={"Checkout"} />
     <Container>
       <p className='text-header text-base font-dmSans leading-7.5'>Have a coupon? <span className='text-menuHeading'>Click here to enter your code</span></p>
-      <Heading className={"pt-28 font-dmSans font-bold text-[39px] text-menuHeading"} text={"Billing Details"} as={"h2"}/>
+      <Heading className={"pt-28 pb-10 font-dmSans font-bold text-[39px] text-menuHeading"} text={"Billing Details"} as={"h2"}/>
+      <div className="">
+        <Flex className={"gap-x-10"}>
+          <div className="w-127"></div>
+        </Flex>
+      </div>
     </Container>
     </>
   )
