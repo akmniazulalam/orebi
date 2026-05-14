8060ad6f8342fc2efef6bb0044c85d3f2aeca5b7
akmniazulalam <niazulalam097@gmail.com>
2026-05-14 23:59:56 +0600

    state management with zustand

--- src/components/ActiveButtons.jsx
+++ src/components/ActiveButtons.jsx
@@ -1,30 +1,38 @@
-import React from 'react'
-import Flex from './Flex'
+import React from "react";
+import Flex from "./Flex";
 import { FaHeart, FaShoppingCart } from "react-icons/fa";
-import CompareIcon from '../assets/icons/CompareIcon';
-import useCart from '@/store/cart';
+import CompareIcon from "../assets/icons/CompareIcon";
+import useCart from "@/store/cart";
 
-
-const ActiveButtons = ({className, product}) => {
-
-    const addToCart = useCart((state) => state.addToCart)
+const ActiveButtons = ({ className, product }) => {
+  const addToCart = useCart((state) => state.addToCart);
 
   return (
-    <div className={`py-5 px-4.5 bg-white opacity-0 ${className}`} onClick={(e) => e.preventDefault()}>
-        <Flex className={"justify-end"}>
-            <button className='font-dmSans text-base text-header hover:text-menuHeading transition-all duration-300 hover:font-bold cursor-pointer'>Add to Wish List</button>
-            <FaHeart className='text-menuHeading ml-3'/>
-        </Flex>
-        <Flex className={"pt-4 justify-end"}>
-            <button className='font-dmSans text-base text-header pr-3 hover:text-menuHeading transition-all duration-300 hover:font-bold cursor-pointer'>Compare</button>
-            <CompareIcon/>
-        </Flex>
-        <Flex className={"pt-4 justify-end"}>
-            <button className='font-dmSans text-base text-header hover:text-menuHeading transition-all duration-300 hover:font-bold cursor-pointer' onClick={() => addToCart(product)}>Add to Cart</button>
-            <FaShoppingCart className='text-menuHeading ml-3'/>
-        </Flex>
+    <div
+      className={`py-5 px-4.5 bg-white opacity-0 ${className}`}
+      onClick={(e) => e.preventDefault()}>
+      <Flex className={"justify-end"}>
+        <button className="font-dmSans text-base text-header hover:text-menuHeading transition-all duration-300 hover:font-bold cursor-pointer">
+          Add to Wish List
+        </button>
+        <FaHeart className="text-menuHeading ml-3" />
+      </Flex>
+      <Flex className={"pt-4 justify-end"}>
+        <button className="font-dmSans text-base text-header pr-3 hover:text-menuHeading transition-all duration-300 hover:font-bold cursor-pointer">
+          Compare
+        </button>
+        <CompareIcon />
+      </Flex>
+      <Flex className={"pt-4 justify-end"}>
+        <button
+          className="font-dmSans text-base text-header hover:text-menuHeading transition-all duration-300 hover:font-bold cursor-pointer"
+          onClick={() => addToCart(product)}>
+          Add to Cart
+        </button>
+        <FaShoppingCart className="text-menuHeading ml-3" />
+      </Flex>
     </div>
-  )
-}
+  );
+};
 
-export default ActiveButtons
+export default ActiveButtons;
