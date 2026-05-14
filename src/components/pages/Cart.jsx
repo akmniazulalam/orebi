ad1235fd4c41c678de9c68bfc92d1ccb8aab3b56
akmniazulalam <niazulalam097@gmail.com>
2026-05-15 01:13:49 +0600

    font apply globally

--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -20,46 +20,43 @@ import toast from "react-hot-toast";
 import axios from "axios";
 
 const Cart = () => {
-  const {items, removeFromCart, increaseQuantity, decreaseQuantity} = useCart((state) => state);
+  const { items, removeFromCart, increaseQuantity, decreaseQuantity } = useCart(
+    (state) => state,
+  );
   let subTotal = items.reduce(
-    (total, item) => total + (item.price || item.variants[0].price) * item.quantity,
+    (total, item) =>
+      total + (item.price || item.variants[0].price) * item.quantity,
     0,
   );
 
-// coupon states
+  // coupon states
   const [couponCode, setCouponCode] = useState("");
   const [discount, setDiscount] = useState(0);
 
   // apply coupon
   const handleCoupon = async () => {
-  try {
+    try {
+      const res = await axios.post(
+        "https://mern-ecommerce-91cv.onrender.com/api/v1/coupon/apply-coupon",
+        {
+          code: couponCode,
+          subtotal: subTotal,
+        },
+      );
 
-    const res = await axios.post(
-      "https://mern-ecommerce-91cv.onrender.com/api/v1/coupon/apply-coupon",
-      {
-        code: couponCode,
-        subtotal: subTotal,
-      }
-    );
+      setDiscount(res.data.discount);
 
-    setDiscount(res.data.discount);
+      toast.success(res.data.message);
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Something went wrong");
 
-    toast.success(res.data.message);
-
-  } catch (error) {
-
-    toast.error(
-      error.response?.data?.message || "Something went wrong"
-    );
-console.log(error);
-
-    setDiscount(0);
-  }
-};
+      setDiscount(0);
+    }
+  };
 
   // final total
   const total = Math.max(subTotal - discount, 0);
-  
+
   const [quantity, setQuantity] = useState(1);
 
   const handleIncrement = () => {
@@ -72,7 +69,6 @@ console.log(error);
     }
   };
 
-
   // Empty cart
   if (items.length === 0) {
     return (
@@ -81,9 +77,7 @@ console.log(error);
 
         <Container>
           <div className="pb-14 text-center">
-            <h2 className="text-3xl font-bold">
-              Your cart is empty
-            </h2>
+            <h2 className="text-3xl font-bold">Your cart is empty</h2>
 
             <Link to={"/shop"}>
               <button className="mt-5 px-6 py-3 bg-black text-white cursor-pointer">
@@ -125,12 +119,16 @@ console.log(error);
                     <button
                       className="cursor-pointer"
                       onClick={() => {
-                        removeFromCart(item._id || item.id)
+                        removeFromCart(item._id || item.id);
                       }}>
                       <ImCross />
                     </button>
                     <img
-                      src={item.thumbnail || item.image || item.variants[0].images[0]}
+                      src={
+                        item.thumbnail ||
+                        item.image ||
+                        item.variants[0].images[0]
+                      }
                       className="w-20 h-20 bg-[#D8D8D8] ml-10 mx-5"
                     />
                     <div className="text-left space-y-2.5">
@@ -146,24 +144,23 @@ console.log(error);
                 <td className="py-6 px-5">
                   <div className="py-1.5 px-4 w-36 border border-infoBg flex gap-x-10 font-dmSans text-header justify-center">
                     <button
-                      onClick={() =>
-                        decreaseQuantity(item._id || item.id)
-                      }
+                      onClick={() => decreaseQuantity(item._id || item.id)}
                       className="cursor-pointer">
                       -
                     </button>
                     <h6>{item.quantity}</h6>
                     <button
-                      onClick={() =>
-                        increaseQuantity(item._id || item.id)
-                      }
+                      onClick={() => increaseQuantity(item._id || item.id)}
                       className="cursor-pointer">
                       +
                     </button>
                   </div>
                 </td>
                 <td className="text-[20px] font-dmSans font-bold text-menuHeading py-6 px-5">
-                  ${(item.quantity * item.price || item.variants[0].price).toFixed(2)}
+                  $
+                  {(
+                    item.quantity * item.price || item.variants[0].price
+                  ).toFixed(2)}
                 </td>
               </tr>
             </tbody>
@@ -201,12 +198,10 @@ console.log(error);
 
               <Button
                 className="rounded-none cursor-pointer font-dmSans"
-                onClick={handleCoupon}
-              >
+                onClick={handleCoupon}>
                 Apply Coupon
               </Button>
             </Field>
-
           </div>
 
           <button className="font-bold font-dmSans text-sm text-menuHeading cursor-pointer">
@@ -235,7 +230,7 @@ console.log(error);
                 -${discount.toFixed(2)}
               </td>
             </tr>
-            
+
             <tr className="">
               <th className="font-bold font-dmSans text-base text-menuHeading py-4 px-5 w-2xs border-r border-infoBg text-left">
                 Total
