884743ce728927ee0eeabd672d0b05be7be5a980
akmniazulalam <niazulalam097@gmail.com>
2026-05-28 23:55:16 +0600

    unnecessary code remove

--- src/components/layouts/BestSellers.jsx
+++ src/components/layouts/BestSellers.jsx
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Container from "../Container";
 import Image from "../Image";
-import ProductOne from "/src/assets/productOne.png";
-import ProductTwo from "/src/assets/productTwo.png";
-import Cup from "/src/assets/cup.png";
-import ProductThree from "/src/assets/productThree.png";
 import Heading from "../Heading";
 import Badge from "../Badge";
 import ActiveButtons from "../ActiveButtons";
@@ -58,66 +54,6 @@ const BestSellers = () => {
               <Black />
             </div>
           ))}
-          {/* <div>
-            <div className="relative w-full group/img">
-              <Image
-                src={ProductTwo}
-                alt={"ProductTwo"}
-                className={"w-full h-full object-cover"}
-              />
-              <Badge
-                badgeT={"New"}
-                className={"absolute top-[19px] left-[19px]"}
-              />
-              <ActiveButtons
-                className={
-                  "absolute bottom-0 left-0 w-full group-hover/img:opacity-100 transition-all duration-400"
-                }
-              />
-            </div>
-            <ProductTexts />
-            <Black />
-          </div>
-          <div>
-            <div className="relative w-full group/img">
-              <Image
-                src={Cup}
-                alt={"Cup"}
-                className={"w-full h-full object-cover"}
-              />
-              <Badge
-                badgeT={"New"}
-                className={"absolute top-[19px] left-[19px]"}
-              />
-              <ActiveButtons
-                className={
-                  "absolute bottom-0 left-0 w-full group-hover/img:opacity-100 transition-all duration-400"
-                }
-              />
-            </div>
-            <ProductTexts />
-            <Black />
-          </div>
-          <div>
-            <div className="relative w-full group/img">
-              <Image
-                src={ProductThree}
-                alt={"ProductThree"}
-                className={"w-full h-full object-cover"}
-              />
-              <Badge
-                badgeT={"New"}
-                className={"absolute top-[19px] left-[19px]"}
-              />
-              <ActiveButtons
-                className={
-                  "absolute bottom-0 left-0 w-full group-hover/img:opacity-100 transition-all duration-400"
-                }
-              />
-            </div>
-            <ProductTexts />
-            <Black />
-          </div> */}
         </Flex>
       </Container>
     </section>
