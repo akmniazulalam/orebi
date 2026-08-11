99cb9c30eabdb8fd5d6c2f589740ad581e4177a6
akmniazulalam <niazulalam097@gmail.com>
2026-08-11 15:17:17 +0600

    feat: connect customer signup

--- src/lib/productApi.js
+++ src/lib/productApi.js
@@ -21,6 +21,7 @@ export const API_BASE_URL = apiBaseUrl;
 export const apiPaths = {
   auth: {
     login: "/auth/login",
+    signup: "/auth/signup",
     currentUser: "/auth/currentuser",
   },
   products: {
