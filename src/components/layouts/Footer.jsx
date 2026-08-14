b14374764e89ac1bc6e13cf679bb0d39d9225449
akmniazulalam <niazulalam097@gmail.com>
2026-08-14 23:57:52 +0600

    real category fetch in footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -72,7 +72,7 @@ const Footer = () => {
                     </Link>
                     <Link to={"/"}>
                       <li className="font-dmSans text-footerTexts text-[14px] font-normal hover:text-menuHeading hover:font-bold transition-all duration-300">
-                        Category 3
+                        Smartphones
                       </li>
                     </Link>
                     <Link to={"/"}>
