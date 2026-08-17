3e597cd2c862ddba6f6e67aab6adc0bf1c7a540e
akmniazulalam <niazulalam097@gmail.com>
2026-08-17 23:52:45 +0600

    feat: add badge-based product filtering

--- src/components/layouts/SpecialOffers.jsx
+++ src/components/layouts/SpecialOffers.jsx
@@ -86,7 +86,6 @@ const SpecialOffers = () => {
                     />
                   </div>
                   <ProductTexts />
-                  <Black />
                 </div>
               );
             })}
