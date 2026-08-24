bf0f9b0108d598192a021397849398f4584f4291
akmniazulalam <niazulalam097@gmail.com>
2026-08-25 00:16:21 +0600

    fix(ProductTexts): add truncation and responsive width to heading class

--- src/components/ProductTexts.jsx
+++ src/components/ProductTexts.jsx
@@ -7,7 +7,7 @@ const ProductTexts = ({text, price}) => {
       <Flex className={"justify-between items-start pt-6"}>
         <Heading
           text={text}
-          className={"font-dmSans font-bold text-base text-menuHeading"}
+          className={"font-dmSans font-bold text-base text-menuHeading truncate sm:w-80.5 lg:w-73"}
           as={"h4"}
         />
         <p className="font-dmSans text-base text-header">${price}</p>
