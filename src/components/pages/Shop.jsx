137f681575e9211c7daa9653e7933eb8255923af
akmniazulalam <niazulalam097@gmail.com>
2026-09-02 00:14:30 +0600

    feat(Shop): set document title for Shop page using custom hook

--- src/components/pages/Shop.jsx
+++ src/components/pages/Shop.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import usePageTitle from "@/hooks/usePageTitle";
 import Intro from "./../Intro";
 import Container from "../Container";
 import ShopProducts from "../layouts/ShopProducts";
 
 const Shop = () => {
+  usePageTitle("Shop");
   return (
     <>
       <Intro text={"Products"} pText={"Products"} />
